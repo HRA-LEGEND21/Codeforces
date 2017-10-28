9d1bb98e3566b1225939716539b7becf67a68820
Shadow_Breaker <heshamrobert21@gmail.com>
2017-10-28 18:18:02 +0300

    Update Round #257 (Div. 2) - A. Jzzhu and Children.cpp
    my solution

--- Round #257 (Div. 2) - A. Jzzhu and Children.cpp
+++ Round #257 (Div. 2) - A. Jzzhu and Children.cpp
@@ -1,91 +1,64 @@
-/**
-            BISMILLAHIR RAHMANIR RAHIM { IN THE NAME OF ALLAH }
-
-           LLLLLLL      LL     LL   LL    LL   LL    LLLLLLLLLL
-         LL        LL   LL     LL   LL   LL    LL    LL        LL
-         LL             LL     LL   LL  LL     LL    LL        LL
-           LLLLLL       LLLLLLLLL   LLL        LL    LLLLLLLLLL
-                  LLL          LL   LL  LL     LL    LL        LL
-        LL        LLL          LL   LL   LL    LL    LL        LL
-          LLLLLLLLL            LL   LL    LL   LL    LLLLLLLLLLL
-**/
-
-
-// All standard Headers
-#include <algorithm>
-#include <cctype>
-#include <cstdio>
-#include <cstdlib>
-#include <cstring>
 #include <iostream>
-#include <list>
-#include <map>
-#include <math.h>
-#include <queue>
+#include <algorithm>
+#include <cstdio>
 #include <set>
-#include <stack>
+#include <math.h>
+#include <cmath>
+#include <sstream>
 #include <string>
 #include <vector>
+#include <map>
+#include <iterator>
+#include <vector>
+#include <cstdint>
+#include<bits/stdc++.h>
+#define inn freopen("in.in","r",stdin)
+#define outt freopen("out.out","w",stdout)
+#define llp(n) for(ll i=n-1;i>=0;i--)
+#define lp(i,n) for(int i=0;i<(int)n;i++)
+#define INF -1000000000
+#define clr(v,d) memset(v,d,sizeof (v))
+#define sz(v) ((int)(v.size()))
+#define all(v) ((v).begin()), ((v).end())
 
+typedef long long ll;
+typedef unsigned long long ul;
+typedef long double db;
+//typedef pair<int, int> ii;
+//typedef vector<ll> vii;
+//typedef vector<int> vi;
 using namespace std;
 
-// Data type
-typedef long long int LLI;
-typedef long int  LI;
-typedef vector<LLI> VLLI;
-typedef vector<int> VI;
-typedef vector<string> VS;
-typedef map<int,int> MII;
-typedef map<string,int> MSI;
-typedef map<string,string> MSS;
-
-// I/O Defination
-#define SI(n) scanf("%d",&n)
-#define SIL(n) scanf("%lld",&n);
-#define SC(a) scanf("%c",&a)
-#define SS(x) scanf("%s",x)
-#define CPS(s) getline(cin , s)
-#define IP(n) printf("%d",n)
-#define ILP(n) printf("%lld",n)
-#define CPSP(a) cout << a << "\n"
-#define PB push_back
-#define MP make_pair
-#define MSET(a,v) memset(a,v,sizeof(a))
-#define STB(const) const.begin()
-#define STE(const) const.end()
-
-// Program Flow & Mathematical Functions
-#define FOR(i,n,inc) for(;i < n;i += inc)
-#define GCD(a,b) __gcd(a, b)
-#define LCM(a,b) (a/GCD(a,b))*b
-#define PI 3.1415926535897932384626433832795
-
-int main(){
-    int n,m,a,x;
-    queue<int> q,c;
-    cin >> n >> m;
-    int i = 1;
-    while(n--){
-        cin >> a;
-        q.push(a);
-        c.push(i);
-        i++;
+int main()
+{
+    int n,m,x;
+    bool flag=1;
+    scanf("%d %d",&n,&m);
+    queue<int>q,c;
+    lp(i,n)
+    {
+        scanf("%d",&x);
+        flag&=(x<=m);
+        q.push(x);
+        c.push(i+1);
     }
-    int l;
-    while(!q.empty()){
-        l = q.front();
-        x = c.front();
-
-        if(m >= l){
+    int element;
+    if(flag)printf("%d\n",n);
+    else
+    {
+        while(!q.empty())
+        {
+            element = q.front();
+            x = c.front();
             q.pop();
             c.pop();
-        }else{
-            q.pop();
-            q.push(l-m);
-            c.pop();
-            c.push(x);
+            if(m<element)
+            {
+                q.push(element-m);
+                c.push(x);
+            }
         }
+        printf("%d\n",x);
     }
-    cout << x << endl;
     return 0;
 }
